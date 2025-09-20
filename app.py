e866833547d59319987049cf2a6fddc965531b06
pauloarm <149835566+pauloarm@users.noreply.github.com>
2025-09-20 19:19:08 -0300

    fix

--- app.py
+++ app.py
@@ -1053,5 +1053,5 @@ def listar_vendas():
     return render_template('listar_vendas.html', vendas=vendas_com_itens)
     
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    init_db(app)
+    app.run(debug=True)
